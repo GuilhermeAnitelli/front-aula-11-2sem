c42751c73f0d1aa32955a11e176158d8bd010a37
GuilhermeAnitelli <guilherme.anitelli10@gmail.com>
2026-04-27 12:17:41 -0300

    feat: Adiciona navegação e exibição de usuários.

--- aula-11/src/App.tsx
+++ aula-11/src/App.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 export default function App() {
   return (
     <div>
       <header>
         <h1>TEMP</h1>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/users">Users</Link>
+        </nav>
       </header>
 
         <Outlet/>
--- aula-11/src/routes/Users/index.tsx
+++ aula-11/src/routes/Users/index.tsx
@@ -1,8 +1,49 @@
+import { useEffect, useState } from "react";
 
 export default function Users() {
+
+    const [listaUsuarios, setListaUsuarios] = useState([{
+    login:"",
+    id:0,
+    avatar_url:""
+    }]);
+
+
+    useEffect(() => {
+        
+       const chamaUsers = async () => {
+            const usuarios = await fetch("https://api.github.com/users");
+
+            const data = await usuarios.json();
+
+            setListaUsuarios(data);
+
+            console.log(data);
+        }
+        
+        chamaUsers();
+    }, [])
+    
+
+    
+
+    
+
+
   return (
     <main>
         <h1>Users</h1>
+
+        <div>
+
+            <ul>
+                {listaUsuarios.map((u,indice)=>(
+                    <li key={indice}>{u.id} - {u.login} - <img src={u.avatar_url} alt="Face do user" /></li>
+                ))}
+            </ul>
+
+        </div>
+
     </main>
   )
 }
